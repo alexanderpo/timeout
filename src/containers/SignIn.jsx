4a3e23ce6ab1b7b970102d854ccabe94ab258e14
alexanderpo <alexanderpometko@gmail.com>
2017-03-17 12:04:34 +0300

    Implement signin container with redux push action

--- src/containers/SignIn.jsx
+++ src/containers/SignIn.jsx
@@ -1,9 +1,87 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
 import { TextField, RaisedButton } from 'material-ui';
+import _ from 'lodash';
 import Logo from '../components/Logo';
+import { signInValidate } from '../utils/inputValidation';
+
+const propTypes = {
+  actions: PropTypes.shape({
+    push: PropTypes.func,
+  }),
+};
 
 class SignIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      password: '',
+      errorName: '',
+      errorPassword: '',
+      dialogBoxIsOpen: false,
+      dialogBoxText: '',
+    };
+
+    this.handleInputValue = this.handleInputValue.bind(this);
+    this.clearInputFields = this.clearInputFields.bind(this);
+    this.handleKeyPressEnter = this.handleKeyPressEnter.bind(this);
+    this.handleSignIn = this.handleSignIn.bind(this);
+  }
+
+  handleInputValue(key) {
+    return (event) => {
+      const value = event.target.value;
+      this.setState({
+        [key]: value,
+      });
+    };
+  }
+
+  handleKeyPressEnter(event) {
+    if (event.key === 'Enter') {
+      this.handleSignIn();
+    }
+  }
+
+  clearInputFields() {
+    this.setState({
+      name: '',
+      password: '',
+      errorName: '',
+      errorPassword: '',
+    });
+  }
+
+  handleSignIn() {
+    const { name, password } = this.state;
+    const values = { name, password };
+    const errors = signInValidate(values);
+
+    if (!_.isEmpty(errors)) {
+      this.setState({
+        errorName: errors.name,
+        errorPassword: errors.password,
+      });
+    } else {
+      // TODO: implement sign in action
+      this.clearInputFields();
+    }
+  }
+
   render() {
+    const {
+      name,
+      password,
+      errorName,
+      errorPassword,
+    } = this.state;
+
+    const { actions } = this.props;
+
     return (
       <div>
         <div className="sign-wrapper">
@@ -11,22 +89,31 @@ class SignIn extends Component {
           <TextField
             hintText="Имя пользователя"
             floatingLabelText="Имя пользователя"
+            value={name}
+            errorText={errorName}
+            onChange={this.handleInputValue('name')}
+            onKeyPress={this.handleKeyPressEnter}
           />
           <TextField
             hintText="Пароль"
             floatingLabelText="Пароль"
             type="password"
+            value={password}
+            errorText={errorPassword}
+            onChange={this.handleInputValue('password')}
+            onKeyPress={this.handleKeyPressEnter}
           />
           <RaisedButton
-            className="sign-button"
+            className="sign-in-button"
             label="Войти"
             primary={true}
+            onTouchTap={this.handleSignIn}
           />
           <RaisedButton
-            className="sign-button"
+            className="back-sign-up-button"
             label="Зарегистрироваться"
             primary={true}
-            onClick={() => { }}
+            onClick={() => { actions.push('/signup'); }}
           />
         </div>
       </div>
@@ -34,4 +121,10 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn;
+SignIn.propTypes = propTypes;
+
+export default connect(null, dispatch => ({
+  actions: bindActionCreators({
+    push,
+  }, dispatch),
+}))(SignIn);
--- src/utils/inputValidation.js
+++ src/utils/inputValidation.js
@@ -2,11 +2,15 @@ export function signInValidate(values) {
   const errors = {};
 
   if (values.name.length === 0) {
-    errors.name = 'Username is empty';
-  }
-
-  if (values.password.length === 0) {
-    errors.password = 'Password is empty';
+    errors.name = 'Введите имя пользователя';
+  } else if (values.name.length < 3) {
+    errors.name = 'Имя пользователя должно содержать не менее 3 символов';
+  } else if (!/^[A-Za-z0-9-_.]+$/.test(values.name)) {
+    errors.name = 'Имя пользователя может содержать только символы A-Z a-z _ - . 1-9';
+  } else if (values.password.length === 0) {
+    errors.password = 'Введите пароль';
+  } else if (values.password.length < 6) {
+    errors.password = 'Пароль должен содержать не менее 6 символов';
   }
 
   return errors;
